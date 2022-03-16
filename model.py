6007997c0d82d087b7b73ff4c56f489ab3c18842
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-03-16 11:35:40 -0400

    Delete model.py

--- model.py
+++
@@ -1,7 +0,0 @@
-# I am defining a data model for non-mandatory fields to define a base model
-# It defines a data model for an api to pass the dates:
-from pydantic import BaseModel, Field
-
-class QueryDates(BaseModel):
-    fromtime : str = Field(default=None)
-    totime : str = Field(default=None)
